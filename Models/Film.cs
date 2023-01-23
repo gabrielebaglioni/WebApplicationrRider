97ac0a5cca07905b337595a427ae335f4bba1205
Gabriele Baglioni <g.baglioni@DEGHQ.LOCAL>
2023-01-23 01:47:39 +0100

    Initial commit

---
+++ Models/Film.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplicationrRider.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Genre { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
